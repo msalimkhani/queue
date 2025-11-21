457bf2d500385843cd4c7511b291734002e088c1
msalimkhani <m.salimkhani@iau.ir>
2025-11-21 10:17:14 +0330

    remove static folder

---
+++ include/Queue.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef __QUEUE_HPP__
+#define __QUEUE_HPP__
+
+#include <linkedlist/LinkedList.hpp>
+
+namespace DataStructures
+{
+    template <typename T>
+    class Queue
+    {
+    private:
+        /* data */
+    public:
+        Queue(/* args */);
+        ~Queue();
+    };
+    
+    Queue::Queue(/* args */)
+    {
+    }
+    
+    Queue::~Queue()
+    {
+    }
+    
+} // namespace DataStructures
+
+
+#endif
